c98d46219073d83021115977e304b939dd7a8e01
ruiqi guo <guoruiqi01@163.com>
2023-11-19 16:07:25 +0800

    调试bootstrap+创建导航栏

---
+++ web/src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">King of Bot</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav" style="margin-right: 80%">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">对战</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">对局列表</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">排行榜</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              guoruiqi
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">我的Bot</a></li>
+              <li><hr class="dropdown-divider" /></li>
+              <li><a class="dropdown-item" href="#">退出</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
