3218ed461da3c3a30d86370348ae62dd070e682d
ruiqi guo <1811319761@qq.com>
2024-01-01 16:32:48 +0800

    创建用户注册页面

--- web/src/views/user/account/UserAccountRegisterView.vue
+++ web/src/views/user/account/UserAccountRegisterView.vue
@@ -1,15 +1,96 @@
 <template>
-  <ContentField> 用户注册 </ContentField>
+  <ContentField>
+    <div class="row justify-content-md-center">
+      <div class="col-3">
+        <form @submit.prevent="login">
+          <div class="mb-3">
+            <label for="username" class="form-label">用户名</label>
+            <input
+              v-model="username"
+              type="text"
+              class="form-control"
+              id="username"
+              placeholder="请输入用户名"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">密码</label>
+            <input
+              v-model="password"
+              type="password"
+              class="form-control"
+              id="password"
+              placeholder="请输入密码"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="confirmedPassword" class="form-label">确认密码</label>
+            <input
+              v-model="confirmedPassword"
+              type="password"
+              class="form-control"
+              id="confirmedPassword"
+              placeholder="请确认密码"
+            />
+          </div>
+          <div class="error-message">{{ error_message }}</div>
+          <button type="submit" class="btn btn-primary" @click="register">
+            提交
+          </button>
+        </form>
+      </div>
+    </div>
+  </ContentField>
 </template>
 
 <script>
 import ContentField from "@/components/ContentField";
+import { ref } from "vue";
+import $ from "jquery";
+import router from "@/router";
+
 export default {
   components: {
     ContentField,
   },
+  setup() {
+    let username = ref("");
+    let password = ref("");
+    let confirmedPassword = ref("");
+    let error_message = ref("");
+
+    const register = () => {
+      $.ajax({
+        url: "http://localhost:3000/user/account/register/",
+        type: "post",
+        data: {
+          username: username.value,
+          password: password.value,
+          confirmedPassword: confirmedPassword.value,
+        },
+        success(resp) {
+          console.log(resp);
+          if (resp.error_message === "success") {
+            router.push({ name: "user_account_login" });
+          } else {
+            error_message.value = resp.error_message;
+          }
+        },
+      });
+    };
+    return {
+      username,
+      password,
+      confirmedPassword,
+      error_message,
+      register,
+    };
+  },
 };
 </script>
 
-<style>
+<style scoped>
+div.error_message {
+  color: red;
+}
 </style>
