5e671421026ea57034b6157952f38129eed47485
ruiqi guo <1811319761@qq.com>
2024-01-09 17:10:01 +0800

    Bot-AddService

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -4,9 +4,33 @@
 
 <script>
 import ContentField from "@/components/ContentField";
+import $ from "jquery";
+import { useStore } from "vuex";
+
 export default {
   components: {
     ContentField,
+  },
+  setup() {
+    const store = useStore();
+    $.ajax({
+      url: "http://localhost:3000/user/bot/add/",
+      type: "post",
+      data: {
+        title: "Bot的标题",
+        content: "Bot的内容",
+        description: "Bot的描述",
+      },
+      headers: {
+        Authorization: "Bearer " + store.state.user.token,
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      },
+    });
   },
 };
 </script>
