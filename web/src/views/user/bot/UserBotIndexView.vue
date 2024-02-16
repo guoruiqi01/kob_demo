afaec29c78c47c7df043575e1a8b403249ec273a
ruiqi guo <1811319761@qq.com>
2024-02-16 09:15:39 +0800

    按钮绑定Modal使用Add

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -16,9 +16,92 @@
               type="button"
               class="btn btn-primary float-end"
               style="width: 100px"
+              data-bs-toggle="modal"
+              data-bs-target="#add-bot-btn"
             >
               创建Bot
             </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="add-bot-btn" tabindex="-1">
+              <div class="modal-dialog modal-xl">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                      创建Bot
+                    </h1>
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                  </div>
+                  <div class="modal-body">
+                    <form>
+                      <div class="mb-3">
+                        <label for="add-bot-title" class="form-label"
+                          >名称</label
+                        >
+                        <input
+                          type="text"
+                          class="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="请输入Bot名称"
+                          v-model="addbot.title"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="add-bot-description" class="form-label"
+                          >简介</label
+                        >
+                        <textarea
+                          type="password"
+                          class="form-control"
+                          id="add-bot-description"
+                          placeholder="请输入Bot简介"
+                          rows="3"
+                          v-model="addbot.description"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="add-bot-description" class="form-label"
+                          >代码</label
+                        >
+                        <textarea
+                          type="password"
+                          class="form-control"
+                          id="add-bot-description"
+                          placeholder="请输入Bot代码"
+                          rows="8"
+                          v-model="addbot.content"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <div class="error-message">{{ addbot.error_message }}</div>
+                    <button
+                      @click="add_bot"
+                      type="button"
+                      class="btn btn-primary"
+                      style="width: 100px"
+                    >
+                      创建
+                    </button>
+                    <button
+                      type="button"
+                      class="btn btn-secondary"
+                      data-bs-dismiss="modal"
+                      style="width: 100px"
+                    >
+                      取消
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="card-body">
             <table class="table table-striped table-hover">
@@ -62,13 +145,21 @@
 <script>
 import $ from "jquery";
 import { useStore } from "vuex";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+import { Modal } from "bootstrap/dist/js/bootstrap";
 export default {
   components: {},
   setup() {
     const store = useStore();
 
     let bots = ref([]);
+
+    const addbot = reactive({
+      title: "",
+      description: "",
+      content: "",
+      error_message: "",
+    });
 
     const refresh_bots = () => {
       $.ajax({
@@ -83,10 +174,39 @@ export default {
       });
     };
 
+    const add_bot = () => {
+      addbot.error_message = "";
+      $.ajax({
+        url: "http://localhost:3000/user/bot/add/",
+        type: "post",
+        data: {
+          title: addbot.title,
+          description: addbot.description,
+          content: addbot.content,
+        },
+        headers: {
+          Authorization: "Bearer " + store.state.user.token,
+        },
+        success(resp) {
+          if (resp.error_message === "success") {
+            addbot.title = "";
+            addbot.description = "";
+            addbot.content = "";
+            Modal.getInstance("#add-bot-btn").hide();
+            refresh_bots();
+          } else {
+            addbot.error_message = resp.error_message;
+          }
+        },
+      });
+    };
+
     refresh_bots();
 
     return {
       bots,
+      addbot,
+      add_bot,
     };
 
     // $.ajax({
