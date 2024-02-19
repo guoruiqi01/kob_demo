81ee72f693b42fa2686218e2ab11df088f4d41ef
ruiqi guo <1811319761@qq.com>
2024-02-19 16:17:57 +0800

    Match-前端

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,14 +1,17 @@
 <template>
-  <PlayGround />
+  <PlayGround v-if="$store.state.pk.status === 'playing'" />
+  <MatchGround v-if="$store.state.pk.status === 'matching'" />
 </template>
 
 <script>
 import PlayGround from "@/components/PlayGround";
+import MatchGround from "@/components/MatchGround";
 import { onMounted, onUnmounted } from "vue";
 import { useStore } from "vuex";
 export default {
   components: {
     PlayGround,
+    MatchGround,
   },
   setup() {
     const store = useStore();
@@ -16,6 +19,12 @@ export default {
 
     let socket = null;
     onMounted(() => {
+      store.commit("updateOpponent", {
+        username: "我的对手",
+        photo:
+          "https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png",
+      });
+
       socket = new WebSocket(socketUrl);
 
       socket.onopen = () => {
